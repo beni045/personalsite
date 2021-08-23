d7b95257f6531936d0c66906f69ac989a1cfee15
beni045 <benigans45@gmail.com>
2021-08-23 11:43:52 -0700

    add project pages

--- pages/rccar/script.js
+++ pages/rccar/script.js
@@ -1,12 +1,7 @@
 $(document).ready(function(){
+    $('.navbar').addClass("sticky");
+
     $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        
         // scroll-up button show/hide script
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
@@ -34,38 +29,9 @@ $(document).ready(function(){
     });
 
 
-    // // owl carousel script
-    // $('.carousel').owlCarousel({
-    //     margin: 20,
-    //     loop: true,
-    //     autoplay: true,
-    //     autoplayTimeOut: 2000,
-    //     autoplayHoverPause: true,
-    //     responsive: {
-    //         0:{
-    //             items: 1,
-    //             nav: false
-    //         },
-    //         600:{
-    //             items: 2,
-    //             nav: false
-    //         },
-    //         1000:{
-    //             items: 3,
-    //             nav: false
-    //         }
-    //     }
-    // });
-
     $('.email-btn').click(function(){
-        /* Get the text field */
         var copyText = "b.hegedus45@gmail.com";
       
-        // /* Select the text field */
-        // copyText.select();
-        // copyText.setSelectionRange(0, 99999); /* For mobile devices */
-      
-        /* Copy the text inside the text field */
         navigator.clipboard.writeText(copyText);
         var obj = document.getElementById("email-clipboard");
         var orig = obj.style.color;
@@ -74,13 +40,5 @@ $(document).ready(function(){
              obj.style.color = orig;
         }, 500);
 
-    });
-
-
-    $('.hockey-project').click(function(){
-        window.location='pages/hockey/hockeyrobot.html';
-    });
-    $('.gesturecare-project').click(function(){
-        window.location='pages/rccar/rccar.html';
     });
 });
